6c6e2e6fb05b0d67d72e61653a2e9d8798caf812
Cassandra <uncertainkitten@gmail.com>
2018-10-08 14:00:39 -0700

    Add Root and App components, connect store and Root to entry

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as SessionUtil from './util/session_api_util';
+import Root from './components/root';
 import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
   const root = document.getElementById("root");
-
-  window.postUser = SessionUtil.postUser;
-  window.postSession = SessionUtil.postSession;
-  window.deleteSession = SessionUtil.deleteSession;
+  
+  //Tests
   window.getState = store.getState;
   window.dispatch = store.dispatch;
+  // Test end
 
-
-  ReactDOM.render(<h1>You're on REACT NOW my dude!!!111</h1>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 });
---
+++ frontend/components/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>Oh my god it's APP COMPONENT in R-E-A-C-T!!!</h1>
+  </div>
+);
+
+export default App;
