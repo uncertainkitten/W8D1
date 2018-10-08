5392770c1ca7d5dc8feaea09430e69b229c3f5ab
Cassandra <uncertainkitten@gmail.com>
2018-10-08 13:39:45 -0700

    Create reducers for errors, entities, and the root reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
 import UsersReducer from './users_reducer';
 
-const EntitiesReducer = combineReducers({
+const entitiesReducer = combineReducers({
   users: UsersReducer
 });
 
-export default EntitiesReducer;
+export default entitiesReducer;
