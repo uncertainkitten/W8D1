4847fd74aafdf056d395b6f93f8b81b49d76876d
Cassandra <uncertainkitten@gmail.com>
2018-10-08 13:48:37 -0700

    Finish phase 1 Bench BnB setup - user auth utils, actions, reducers, and store for entry

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as SessionUtil from './util/session_api_util';
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore();
+  const root = document.getElementById("root");
+
   window.postUser = SessionUtil.postUser;
   window.postSession = SessionUtil.postSession;
   window.deleteSession = SessionUtil.deleteSession;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 
-  const root = document.getElementById("root");
+
   ReactDOM.render(<h1>You're on REACT NOW my dude!!!111</h1>, root);
 });
